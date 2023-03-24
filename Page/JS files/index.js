eae8f6df97d6b57955ae5b4a69c2b0b033ee5d1c
yogeshwari-selvendran <syogeshwarijune28@gmail.com>
2023-03-24 18:42:57 +0530

    fix: some bugs in user crud

---
+++ Page/JS files/index.js
@@ -0,0 +1,44 @@
+var project = setInterval(projectDone, 10)
+var cities = setInterval(happyCities, 10)
+var service = setInterval(vehicle, 10)
+var rodeside = setInterval(clients, 10)
+let count1 = 1;
+let count2 = 1;
+let count3 = 1;
+let count4 = 1;
+
+function projectDone() {
+    count1++;
+    document.querySelector("#number1").innerHTML = count1;
+
+    if (count1 == 500) {
+        clearInterval(project)
+    }
+}
+
+function happyCities() {
+    count2++;
+    document.querySelector("#number2").innerHTML = count2;
+
+    if (count2 == 180) {
+        clearInterval(cities)
+    }
+}
+
+function vehicle() {
+    count3++;
+    document.querySelector("#number3").innerHTML = count3;
+
+    if (count3 == 1300) {
+        clearInterval(service)
+    }
+}
+
+function clients() {
+    count4++;
+    document.querySelector("#number4").innerHTML = count4;
+
+    if (count4 == 1500) {
+        clearInterval(rodeside)
+    }
+}
---
+++ Page/JS files/user-signup.js
@@ -0,0 +1,122 @@
+
+
+const formsData = document.getElementById("form-page1")
+formsData.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getData();
+})
+function getData() {
+
+    //This is for get a data in form
+    const firstName = document.getElementById("user-firstname").value;
+    const lastName = document.getElementById("user-lastname").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const email = document.getElementById("user-email").value;
+    const password = document.getElementById("user-password").value;
+    const number = document.getElementById("user-number").value;
+    const address = document.getElementById("user-address").value;
+    const pincodeAddress = document.getElementById("user-pincode").value;
+    const password2 = document.getElementById("confirm-password").value;
+
+    //This is for store data in localstorage
+    let userRecords = new Array();
+    userRecords = JSON.parse(localStorage.getItem("user_data")) ?
+        JSON.parse(localStorage.getItem("user_data")) : []
+    if (userRecords.some((y) => {return y.user_email == email
+    })) {
+        alert("email id is already taken")
+    }
+     else{
+        userRecords.push({
+         "user_Firstname": firstName,
+            "user_lastname": lastName,
+            "gender": gender,
+            "user_email": email,
+            "password": password,
+            "mobile": number,
+            "address1": address,
+            "pinaddress": pincodeAddress
+
+        })
+        window.location.href = "../create_accound/signin.html"
+    }
+    localStorage.setItem("user_data",JSON.stringify(userRecords));
+}
+    // arr = JSON.parse(localStorage.getItem("array")) ?? []
+
+    // if (arr == '') {
+
+    //     let data = {
+    //         "user_Firstname": firstName,
+    //         "user_lastname": lastName,
+    //         "gender": gender,
+    //         "email": email,
+    //         "password": password,
+    //         "mobile": number,
+    //         "address1": address,
+    //         "pinaddress": pincodeAddress
+    //     }
+    //     arr.push(data);
+
+    //     localStorage.setItem("array", JSON.stringify(arr))
+
+    // }
+
+    // else {
+    //     check = JSON.parse(localStorage.getItem("array"))
+
+    //     check.some(e => {
+
+
+    //         if (e["email"] == email) {
+
+    //             res = 1
+    //         }
+    //     })
+    //     if (res == 1) {
+    //         alert("email id already exist")
+
+    //     }
+
+
+    //     // if(password !== password2){
+    //     //     alert("password is not same")
+    //     // }
+    //     else {
+    //         let data = {
+    //             "user_Firstname": firstName,
+    //             "user_lastname": lastName,
+    //             "gender": gender,
+    //             "user_email": email,
+    //             "password": password,
+    //             "mobile": number,
+    //             "address1": address,
+    //             "pinaddress": pincodeAddress
+    //         }
+    //         arr.push(data);
+
+    //         localStorage.setItem("array", JSON.stringify(arr))
+    //     }
+    //     // if(password == password2){
+    //     //     let data = {
+    //     //         "user_Firstname": firstName,
+    //     //         "user_lastname": lastName,
+    //     //         "gender": gender,
+    //     //         "email": email,
+    //     //         "password": password,
+    //     //         "mobile": number,
+    //     //         "address1": address,
+    //     //         "pinaddress": pincodeAddress
+    //     //     }
+    //     //     arr.push(data);
+
+    //     //     localStorage.setItem("array", JSON.stringify(arr))
+    //     // }
+    //     // else{
+    //     //     alert("password is not same")
+    //     // }
+    //     window.location.href = "../create accound/signin.html"
+    // }
+
+
+
