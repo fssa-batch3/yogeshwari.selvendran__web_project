e2d2de3f251ce50dbb0e5dbda58556567f269e09
yogeshwari-selvendran <syogeshwarijune28@gmail.com>
2023-05-18 10:02:12 +0530

    fix: some bug

--- main.js
+++ main.js
@@ -4,162 +4,108 @@ if (JSON.parse(localStorage.getItem("product_detail"))) {
 else {
     localStorage.setItem("product_detail",JSON.stringify([
         {
-            about_product
-                :
-                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-            description
-                :
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
-            highlights
-                :
-                "There are many variations of passages of Lorem Ipsum available,",
-            pr_img_1
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_2
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_3
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_4
-                :
-                "https://iili.io/HXVxUnp.png",
-            product_name
-                :
-                "Bike part",
-            product_price
-                :
-                "600",
-            productid
-                :
-                42,
-            rating
-                :
-                "3",
-            status
-                :
-                true
-        }
-        ,
-
-
-        {
-            about_product
-                :
-                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-            description
-                :
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
-            highlights
-                :
-                "There are many variations of passages of Lorem Ipsum available,",
-            pr_img_1
-                :
-                "https://iili.io/HvQR5Is.png",
-            pr_img_2
-                :
-                "https://iili.io/HvQR5Is.png",
-            pr_img_3
-                :
-                "https://iili.io/HvQR5Is.png",
-            pr_img_4
-                :
-                "https://iili.io/HvQR5Is.png",
-            product_name
-                :
-                "Oil breake part",
-            product_price
-                :
-                "1200",
-            productid
-                :
-                49,
-            rating
-                :
-                "3",
-            status
-                :
-                true
+            "productid": 43,
+            "product_name": "",
+            "about_product": "Mukut-rear-brake-disc-caliper-for-suzuki-gixxer",
+            "product_price": "2,250.00",
+            "pr_img_1": "https://iili.io/HU7lwyg.png",
+            "pr_img_2": "https://iili.io/HU7lwyg.png",
+            "pr_img_3": "https://iili.io/HU7lwyg.png",
+            "pr_img_4": "https://iili.io/HU7lwyg.png",
+            "highlights": "Brand: MUKUT position: Rear Weight: 850g approx. Meterial: Aluminium Alloy ",
+            "rating": "3",
+            "description": "Mukut is a leading aftermarket spare parts manufacturer for over a decade. it specializes in manufacturing of Disc breake Calipers. Its products are trusted by customers all over India.",
+            "status": true
         },
         {
-            about_product
-                :
-                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-            description
-                :
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
-            highlights
-                :
-                "There are many variations of passages of Lorem Ipsum available,",
-            pr_img_1
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_2
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_3
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_4
-                :
-                "https://iili.io/HXVxUnp.png",
-            product_name
-                :
-                "Bike Part 1",
-            product_price
-                :
-                "1200",
-            productid
-                :
-                84,
-            rating
-                :
-                "3",
-            status
-                :
-                true
-        }, {
-            about_product
-                :
-                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-            description
-                :
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
-            highlights
-                :
-                "There are many variations of passages of Lorem Ipsum available,",
-            pr_img_1
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_2
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_3
-                :
-                "https://iili.io/HXVxUnp.png",
-            pr_img_4
-                :
-                "https://iili.io/HXVxUnp.png",
-            product_name
-                :
-                "Bike Part 1",
-            product_price
-                :
-                "1200",
-            productid
-                :
-                84,
-            rating
-                :
-                "3",
-            status
-                :
-                true
-
+            "productid": 75,
+            "product_name": "",
+            "about_product": "mukut's strong and heavy-duty disc brake caliper for robust and smooth braking while riding your bikes",
+            "product_price": "6000",
+            "pr_img_1": "https://iili.io/HU5YTVS.png",
+            "pr_img_2": "https://iili.io/HU5YTVS.png",
+            "pr_img_3": "https://iili.io/HU5YTVS.png",
+            "pr_img_4": "https://iili.io/HU5YTVS.png",
+            "highlights": "Brand: MUKUT position: Rear Weight: 850g approx. Meterial: Aluminium Alloy ",
+            "rating": "3",
+            "description": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English",
+            "status": true
+        },
+        {
+            "productid": 58,
+            "product_name": "mukut-front-brake-disc-caliper-for-bajaj-pulsar-200-ns",
+            "about_product": "mukut-rear-brake-disc-caliper-for-yamaha-r15-v3-with-bracket",
+            "product_price": "2,380.00",
+            "pr_img_1": "https://iili.io/HU707Pp.png",
+            "pr_img_2": "https://iili.io/HU707Pp.png",
+            "pr_img_3": "https://iili.io/HU707Pp.png",
+            "pr_img_4": "https://iili.io/HU707Pp.png",
+            "highlights": "Brand: MUKUT position: Rear Weight: 850g approx. Meterial: Aluminium Alloy ",
+            "rating": "4",
+            "description": "Mukut is a leading aftermarket spare parts manufacturer for over a decade. it specializes in manufacturing of Disc breake Calipers. Its products are trusted by customers all over India.",
+            "status": true
+        },
+        {
+            "productid": 95,
+            "product_name": "",
+            "about_product": "mukut-front-brake-disc-caliper-for-bajaj-pulsar-200-ns",
+            "product_price": "1749:00",
+            "pr_img_1": "https://iili.io/HUSBxTv.png",
+            "pr_img_2": "https://iili.io/HUSBxTv.png",
+            "pr_img_3": "https://iili.io/HUSBxTv.png",
+            "pr_img_4": "https://iili.io/HUSBxTv.png",
+            "highlights": "Brand: MUKUT position: Front Weight: 850g approx. Meterial: Aluminium Alloy ",
+            "rating": "4",
+            "description": "Mukut is a leading aftermarket spare parts manufacturer for over a decade. it specializes in manufacturing of Disc breake Calipers. Its products are trusted by customers all over India.",
+            "status": true
+        },
+        {
+            "productid": 87,
+            "product_name": "",
+            "about_product": "original-honda-front-brake-disc-caliper-for-honda-hornet-160-r-front",
+            "product_price": "4,650:00",
+            "pr_img_1": "https://iili.io/HUSBNyb.png",
+            "pr_img_2": "https://iili.io/HUSBNyb.png",
+            "pr_img_3": "https://iili.io/HUSBNyb.png",
+            "pr_img_4": "https://iili.io/HUSBNyb.png",
+            "highlights": "Brand: original honda position: Front Weight: 850g approx. Meterial: Aluminium Alloy",
+            "rating": "3",
+            "description": "Mukut is a leading aftermarket spare parts manufacturer for over a decade. it specializes in manufacturing of Disc breake Calipers. Its products are trusted by customers all over India.",
+            "status": true
+        },
+        {
+            "productid": 53,
+            "product_name": "",
+            "about_product": "mukut-digital-speedometer-for-bajaj-pulsar-200-ns",
+            "product_price": "3,900.00",
+            "pr_img_1": "https://iili.io/HUSC5oQ.png",
+            "pr_img_2": "https://iili.io/HUSC5oQ.png",
+            "pr_img_3": "https://iili.io/HUSC5oQ.png",
+            "pr_img_4": "https://iili.io/HUSC5oQ.png",
+            "highlights": "Brand: mukut speedometer type: digital Weight: 1 Kg approx. Meterial: PVC + Glass",
+            "rating": "3",
+            "description": "Mukut is a leading aftermarket spare parts manufacturer for over a decade. it specializes in manufacturing of Disc breake Calipers. Its products are trusted by customers all over India.",
+            "status": true
+        },
+        {
+            "productid": 90,
+            "product_name": "",
+            "about_product": "rolon-chain-sprocket-kit-for-bajaj-avenger-150-street-200-6hole-38t-14t-110",
+            "product_price": "2,086.00",
+            "pr_img_1": "https://iili.io/HUSCXxR.png",
+            "pr_img_2": "https://iili.io/HUSCXxR.png",
+            "pr_img_3": "https://iili.io/HUSCXxR.png",
+            "pr_img_4": "https://iili.io/HUSCXxR.png",
+            "highlights": "Brand: rolon contains: rear sprocket(1N),Front sprocket(1N), Chain(1N) Meterial: alloy steel",
+            "rating": "4",
+            "description": "Mukut is a leading aftermarket spare parts manufacturer for over a decade. it specializes in manufacturing of Disc breake Calipers. Its products are trusted by customers all over India.",
+            "status": true
         }
-    ]))
-}
+    ]
+   
+   
+        )
+        )};
 
 
 // let products = [
